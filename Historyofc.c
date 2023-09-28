3e622116da621aa86d3017927db941ec503dd0ad
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-09-28 08:04:38 +0530

    Add files via upload

---
+++ Historyofc.c
@@ -0,0 +1,37 @@
+
+/* C is a general-purpose, procedural, high-level programming language.
+
+    C language was developed by Dennis M. Ritchie (डेनिस एम. रिची ) at 
+    the Bell Telephone Laboratories( U.S.A.) in 1972.
+
+    It is a powerful and flexible language which was first developed 
+    for the programming of the UNIX operating System. 
+
+    A procedure is known as a function, method, routine, subroutine, etc.
+
+    A procedural language specifies a series of steps for the program to
+    solve the problem.
+
+    It used in development of computer software and application,system 
+    - programming , games and web development.
+
+    C is one of the most widely used programming language.
+
+    It is the best choice to start with programming as 
+    it gives you a foundational understanding of programming.
+
+    C is very fast, compared to other programming languages, like Java and Python.
+
+    The main difference between C and C++ is that C++ support classes and objects,
+    while C does not.
+
+    C language is considered as the mother language of all the modern 
+    programming languages because most of the compilers, JVMs, Kernels, etc.
+    are written in C language, and most of the programming languages 
+    follow C syntax, for example, C++, Java, C#, etc.
+
+    A structured programming language is a subset of the procedural language.
+    Structure means to break a program into parts or blocks so that it may be easy to understand.
+
+*/
+
---
+++ Intro_part.c
@@ -0,0 +1,24 @@
+//To write the first c program.
+
+
+#include <stdio.h>// Header file 
+//includes the standard input output library functions.
+// The printf() function is defined in stdio.h .
+
+
+
+int main(){
+//The main() function is the entry point of every program in c language.
+//where the execution of program begins
+
+
+
+    printf("Hello World");
+    //The printf() function is used to print data on the terminal.
+    //statement
+    // semicolon(;) is always define end of the statement or terminate.
+
+    return 0;
+    // The return 0 statement, returns execution status to the OS. 
+    // The 0 value is used for successful execution and 1 for unsuccessful execution.
+}
